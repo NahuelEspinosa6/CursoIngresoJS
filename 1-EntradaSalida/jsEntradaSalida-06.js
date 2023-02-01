b770e27318e2b0beeb35e45a6101785ec4cc0587
NahuelEspinosa6 <nahuelespinosa6@gmail.com>
2023-02-01 12:08:39 -0300

    ejercicio hecho

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,20 +4,24 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	// declaro variables
 	let numUnoTexto;
 	let numDosTexto;
 	let numUno;
 	let numDos;
 	let mensaje;
 
+	// tomo el dato y lo guardo
 	numUnoTexto = document.getElementById("txtIdNumeroUno").value;
 	numDosTexto = document.getElementById("txtIdNumeroDos").value;
 
+	// parseo el dato a numerico
 	numUno = parseInt(numUnoTexto);
 	numDos = parseInt(numDosTexto);
 
+	// concateno el texto y realizo la operacion aritmetica
 	mensaje = "La suma es " + (numUno + numDos);
 
-	alert(mensaje);
+	alert(mensaje); // muestro el mensaje (salida)
 }
 
