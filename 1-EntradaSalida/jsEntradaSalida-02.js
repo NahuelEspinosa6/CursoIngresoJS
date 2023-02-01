6f51fa3e7254caf58a4f269073f77b74fd213c9f
NahuelEspinosa6 <nahuelespinosa6@gmail.com>
2023-02-01 09:52:38 -0300

    ejercicios hechos

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -6,5 +6,13 @@ function mostrar()
 	let nombrePersona; //declaro variable
 	nombrePersona = prompt ("¿Cuál es tu nombre?"); //pido el nombre con prompt
 	alert(nombrePersona); // muestra el mensaje al final
+
+	/*let nombrePersona;
+	let apellidoPersona;
+
+	nombrePersona = prompt ("¿Cual es tu nombre?");
+	apellidoPersona = prompt ("¿Cual es tu apellido?");
+
+	alert("Su nombre es " + nombrePersona + " y su apellido " + apellidoPersona); //concatenamos los textos */
 }
 
