96f3ebab9a09a090950ec46ca1f0d8d85b3240c1
NahuelEspinosa6 <nahuelespinosa6@gmail.com>
2023-02-02 07:47:06 -0300

    ejer10 bis

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,7 +5,7 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	let sueldo;
+	/*let sueldo;
 	let descuento;
 	let sueldoTotal;
 
@@ -15,6 +15,25 @@ function mostrarAumento()
 	descuento = (sueldo * 0.25);
 	sueldoTotal = sueldo - descuento;
 
-	document.getElementById("txtIdResultado").value = sueldoTotal;
+	document.getElementById("txtIdResultado").value = sueldoTotal;*/
+
+	// EJERCICIO 10 BIS
+	/* Ejercicio 10.bis: Debemos lograr tomar el importe por ID.
+	Transformarlo a entero (parseInt), luego
+	pedirle al usuario un % de descuento y mostrar el importe en el cuadro de texto "RESULTADO" */
+
+	let importe;
+	let descuento;
+	let importeFinal;
+
+	importe = document.getElementById("txtIdImporte").value;
+	importe = parseInt(importe);
+
+	descuento = prompt("ingrese el descuento");
+	descuento = parseInt(descuento);
+
+	importeFinal = importe - (importe * descuento/100);
+
+	document.getElementById("txtIdResultado").value = importeFinal;
 
 }
