62f02f1e6c02978bb0498a5b70e3e0ba90211d5a
NahuelEspinosa6 <nahuelespinosa6@gmail.com>
2023-02-02 08:56:04 -0300

    ejercicio resuelto

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -1,17 +1,72 @@
-/*1.	Para el departamento de facturación:
+/*1. Nahuel Espinosa Fuentes	
+    Para el departamento de facturación:
 A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	let productoUno;
+    let productoDos;
+    let productoTres;
+    let mensaje;
+
+    productoUno = document.getElementById("txtIdPrecioUno").value;
+    productoDos = document.getElementById("txtIdPrecioUno").value;
+    productoTres = document.getElementById("txtIdPrecioUno").value;
+
+    productoUno = parseInt(productoUno);
+    productoDos = parseInt(productoDos);
+    productoTres = parseInt(productoTres);
+
+    mensaje = "Precio sin IVA:$" + (productoUno + productoDos + productoTres);
+
+    alert(mensaje);
+
 }
 function Promedio () 
 {
-	
+	let productoUno;
+    let productoDos;
+    let productoTres;
+    let promedio
+    let mensaje;
+
+    productoUno = document.getElementById("txtIdPrecioUno").value;
+    productoDos = document.getElementById("txtIdPrecioUno").value;
+    productoTres = document.getElementById("txtIdPrecioUno").value;
+
+    productoUno = parseInt(productoUno);
+    productoDos = parseInt(productoDos);
+    productoTres = parseInt(productoTres);
+
+    promedio = (productoUno + productoDos + productoTres) / 3;
+
+    mensaje = "Promedio de los productos:$" + promedio;
+
+    alert(mensaje);
 }
 function PrecioFinal () 
 {
-	
+	let productoUno;
+    let productoDos;
+    let productoTres;
+    let iva;
+    let preciofinal;
+    let mensaje;
+
+    productoUno = document.getElementById("txtIdPrecioUno").value;
+    productoDos = document.getElementById("txtIdPrecioUno").value;
+    productoTres = document.getElementById("txtIdPrecioUno").value;
+
+    productoUno = parseInt(productoUno);
+    productoDos = parseInt(productoDos);
+    productoTres = parseInt(productoTres);
+
+    preciofinal = productoUno + productoDos + productoTres;
+    iva = preciofinal + (preciofinal * 0.21)
+
+    mensaje = "Precio Final con IVA:$" + iva;
+
+    alert(mensaje);
 }
