10ec367b1c173cdcc38168bc3f172520780c9987
NahuelEspinosa6 <nahuelespinosa6@gmail.com>
2023-02-02 06:37:06 -0300

    ejercicios terminados

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -1,10 +1,20 @@
-/*
+/* Nahuel Espinosa Fuentes
 Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	let sueldo;
+	let aumento;
+	let sueldoTotal;
 
-	alert("ok");
+	sueldo = document.getElementById("txtIdSueldo").value;
+	sueldo = parseInt(sueldo);
+	
+	aumento = (sueldo * 10/100);
+	sueldoTotal = sueldo + aumento;
+
+	document.getElementById("txtIdResultado").value = sueldoTotal;
+	 
 }
