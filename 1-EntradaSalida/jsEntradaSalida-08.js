6579657faaddf9d217dc64a4de377fbcf4c1c125
NahuelEspinosa6 <nahuelespinosa6@gmail.com>
2023-02-02 07:46:47 -0300

    ejer8 bis

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -5,7 +5,7 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	let dividendoTexto;
+	/*let dividendoTexto;
 	let divisorTexto;
 	let dividendo;
 	let divisor;
@@ -19,5 +19,36 @@ function SacarResto()
 
 	mensaje = "El resto es: " + dividendo % divisor;
 
-	alert(mensaje);
+	alert(mensaje);*/
+
+	// EJERCICIOS 8
+	/* Ejercicio 8.bis: Ingresar dos numeros por id
+	Se pide:
+	mostrar por alert:
+	a) La suma de los dos numeros
+	b) El promedio de los numeros
+	c) El modulo de los numeros (el primero en modulo del segundo) */
+
+	let numUno;
+	let numDos;
+	let mensajeSuma;
+	let mensajePromedio;
+	let mensajeModulo;
+
+	numUno = document.getElementById("txtIdNumeroDividendo").value;
+	numDos = document.getElementById ("txtIdNumeroDivisor").value;
+
+	numUno = parseInt(numUno);
+	numDos = parseInt(numDos);
+
+	// A
+	mensajeSuma = "La suma es " + (numUno + numDos);
+	// B
+	mensajePromedio = "El promedio de los numeros es: " + ((numUno + numDos) / 2);
+	// C
+	mensajeModulo = "El Modulo de ambos numeros es: " + numUno % numDos;
+
+	alert(mensajeSuma);
+	alert(mensajePromedio);
+	alert(mensajeModulo);
 }
