1408118bb5a3efdca565d5618404d50a68110c10
NahuelEspinosa6 <nahuelespinosa6@gmail.com>
2023-02-02 23:28:35 -0300

    ejercicios resueltos

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -1,4 +1,5 @@
-/*2.	Para el departamento de Construcción:
+/*2. Nahuel Espinosa Fuentes	
+    Para el departamento de Construcción:
 A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
@@ -6,13 +7,63 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    // A    Perimetro rectangular (largo * 2) + (ancho * 2)
+    let largo;
+    let ancho;
+    let operacion;
+    let alambre;
+    let mensaje;
+
+    largo = document.getElementById("txtIdLargo").value;
+    ancho = document.getElementById("txtIdAncho").value;
+
+    largo = parseFloat(largo);
+    ancho = parseFloat(largo);
+    
+    operacion = largo * 2 + ancho * 2;
+    alambre = operacion * 3;
+    mensaje = "Cantidad de alambre a comprar: " + alambre;
+
+    alert(mensaje);
 
 }
 function Circulo () 
 {
-	
+	// B    Perimetro del Circulo "2 * pi * Radio "
+    let radio;
+    let operacion;
+    let alambre;
+    let mensaje
+
+    radio = parseFloat(document.getElementById("txtIdRadio").value);
+    operacion = 2 * 3.14 * radio;
+    alambre = (operacion * 2) * 3;
+
+    mensaje = "Cantidad de alambre a comprar: " + alambre;
+
+    alert(mensaje);
 }
 function Materiales () 
 {
-	
+	// C    Area de un rectangulo "largo * ancho"
+    //      "1 mts" area por 3 de cal y 1mts por 2 de cemento
+
+    let largo;
+    let ancho;
+    let operacion;
+    let cal;
+    let cemento;
+    let mensaje;
+
+    largo = parseFloat(document.getElementById("txtIdLargo").value);
+    ancho = parseFloat(document.getElementById("txtIdAncho").value);
+
+    operacion = largo * ancho;
+    cal = operacion * 3;
+    cemento = operacion * 2;
+
+    mensaje = "Necesitas: " + cal + " de cal y " + cemento + " de cemento";
+
+    alert(mensaje);
+
 }
--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -4,10 +4,34 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
-	
+    // Formula ("x"°F - 32) * 5/9 = "Resultado °C"
+    let fahrenheit;
+    let centigrados;
+    let mensaje;
+
+    fahrenheit = document.getElementById("txtIdTemperatura").value;
+    fahrenheit = parseFloat(fahrenheit);
+
+    centigrados = (fahrenheit - 32) * 5/9
+
+    mensaje = fahrenheit + "°F son " + centigrados + "°C";
+
+    alert(mensaje);
 }
 
 function CentigradosFahrenheit () 
 {
-	
+	// Formula ("X"°C * 9/5) + 32 = "Resultado °F"
+    let fahrenheit;
+    let centigrados;
+    let mensaje;
+
+    centigrados = document.getElementById("txtIdTemperatura").value;
+    centigrados = parseFloat(centigrados);
+
+    fahrenheit = (centigrados * 9/5) + 32;
+
+    mensaje = centigrados + "°C son " + fahrenheit + "°F";
+
+    alert(mensaje);
 }
