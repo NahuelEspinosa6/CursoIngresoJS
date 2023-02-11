e69d016c71069fd4cb0ca47d53d9d313a7a454b4
NahuelEspinosa6 <nahuelespinosa6@gmail.com>
2023-02-11 12:10:21 -0300

    ejercicio hecho bis

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,35 @@
 function mostrar()
 {
+	/* al seleccionar un mes informar.
+	si es Enero: "que comiences bien el año!!!."
+	si es Marzo: "a clases!!!."
+	i es Julio: "se vienen las vacaciones!!!."
+	si es Diciembre: "Felices fiesta!!!." */
+
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mesDelAño;
+	let mensaje;
 
+	mesDelAño = document.getElementById("txtIdMes").value;
 
+	switch(mesDelAño){
+		case "Enero":
+			mensaje = "que comiences bien el año!!!"
+			break;
+		case "Marzo":
+			mensaje = "a clases!!!"
+			break;
+		case "Julio":
+			mensaje = "se vienen las vacaciones!!!"
+			break;
+		case "Diciembre":
+			mensaje = "Felices fiesta!!!"
+			break;
+		default:
+			mensaje = "Sos aburrido"
+			break;
+	}
+
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,10 +1,53 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	/* al seleccionar un mes informar.
+	si estamos en Invierno: "Abrigate que hace frio."
+	si aún no llego el Invierno: "Falta para el invierno."
+	si ya paso el Invierno: "Ya pasamos el frio, ahora calor!!!."
+	ACLARAcIÓN: tomamos a Julio y Agosto como los meses de Invierno. */
 
+	let mesDelAño;
+	let mensaje;
 
+	switch(mesDelAño){
+		case "Enero":
+			mensaje = "Falta para el invierno"
+			break;
+		case "Febrero":
+			mensaje = "Falta para el invierno"
+			break;
+		case "Marzo":
+			mensaje = "Falta para el invierno"
+			break;
+		case "Abril":
+			mensaje = "Falta para el invierno"
+			break;
+		case "Mayo":
+			mensaje = "Falta para el invierno"
+			break;
+		case "Junio":
+			mensaje = "Falta para el invierno"
+			break;
+		case "Julio":
+			mensaje = "Abrigate que hace frio"
+			break;
+		case "Agosto":
+			mensaje = "Abrigate que hace frio"
+			break;
+		case "Septiembre":
+			mensaje = "Ya pasamos el frio, ahora calor!!!"
+			break;
+		case "Octubre":
+			mensaje = "Falta para el invierno"
+			break;
+		case "Noviembre":
+			mensaje = "Falta para el invierno"
+			break;
+		case "Diciembre":
+			mensaje = "Falta para el invierno"
+			break;
+	}
 
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
