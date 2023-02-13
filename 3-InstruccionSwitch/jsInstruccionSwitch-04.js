173e3d09f04541bfb6d1e98788e1adf6f5a26923
NahuelEspinosa6 <nahuelespinosa6@gmail.com>
2023-02-13 14:29:11 -0300

    ejercicios resueltos

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,36 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	/* al seleccionar un mes informar.
+	si tiene 28 días.
+	si tiene 30 días.
+	si tiene 31 días. */
 	
+	let mesDelAño;
+	let mensaje;
+
+	mesDelAño = document.getElementById("txtIdMes").value;
 	
+	switch (mesDelAño) {
+		case "Abril":
+		case "Julio":
+		case "Septiembre":
+		case "Noviembre":
+			mensaje = "Tiene 30 dias"
+			break;
+		case"Enero":
+		case"Marzo":
+		case"Mayo":
+		case"Junio":
+		case"Agosto":
+		case"Octubre":
+		case"Diciembre":
+			mensaje = "Tiene 31 dias"
+			break;
+		default:
+			mensaje = "Tiene 28 dias"
+			break;
+	}
 
-
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,27 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	/* Al selecionar un destino , indicar el punto cardinal de nuestro pais en donde se encuentra Norte, Sur, Este u Oeste */
+
+	let destino;
+	let mensaje;
+
+	destino = document.getElementById("txtIdDestino").value;
+
+	switch (destino) {
+		case "Bariloche":
+			mensaje = "Se encuentra al Oeste del Pais"
+			break;
+		case "Cataratas":
+			mensaje = "Se encuentra al Norte del Pais"
+			break;
+		case "Mar del plata":
+			mensaje = "Se encuentra al Este del Pais"
+			break;
+		default:
+			mensaje = "Se encuentra al Sur del Pais"
+			break;
+	}
+
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
