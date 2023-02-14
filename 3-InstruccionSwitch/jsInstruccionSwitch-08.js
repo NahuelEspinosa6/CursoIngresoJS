079ea32eabb399618befe113dcb8bf5d37068215
NahuelEspinosa6 <nahuelespinosa6@gmail.com>
2023-02-14 00:12:36 -0300

    ejercicios resueltos

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,23 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	/* Al seleccionar un destino informar si hace FRIO o CALOR en ese destino */
+
+	let destino;
+	let mensaje;
+
+	destino = document.getElementById("txtIdDestino").value;
+
+	switch (destino) {
+		case "Bariloche":
+		case "Ushuaia":
+			mensaje = "Hace Frio"
+			break;
+		case "Cataratas":
+		case "Mar del plata":
+			mensaje = "Hace Calor"
+			break;
+	}
+
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
