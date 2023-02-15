3787fc97eba23f5ade10bb00574de48144fa1f77
NahuelEspinosa6 <nahuelespinosa6@gmail.com>
2023-02-15 08:41:46 -0300

    ejercicios hechos

--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -8,7 +8,7 @@ function mostrar()
 	numero = prompt("Ingresen un numero entre 0 y 9");
 	numero = parseInt(numero);
 
-	while(numero < -1 || numero > 10){
+	while(isNaN(numero) || numero < -1 || numero > 10){
 		numero = prompt("Error, ingrese un numero entre 0 y 9");
 	}
 
