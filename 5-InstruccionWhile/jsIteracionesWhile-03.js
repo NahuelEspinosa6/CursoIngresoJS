0e67d6899f26a7ccc3b037675e6b8ad304234192
NahuelEspinosa6 <nahuelespinosa6@gmail.com>
2023-02-15 08:39:23 -0300

    ejercicios hechos

--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -3,13 +3,16 @@ al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
 function mostrar()
 {
 	let claveIngresada;
+	let mensaje;
 
 	claveIngresada = prompt("Ingrese la clave");
 
 	while(claveIngresada != "utn750"){
 		claveIngresada = prompt("Error, ingrese la clave nuevamente");
 	}
-
-	alert(claveIngresada);
+	
+	mensaje = "Felicidades inicio de sesion con exito"
+	
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
