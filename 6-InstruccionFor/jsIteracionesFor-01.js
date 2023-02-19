4e723956e00f01b757901383db48825f0114c488
NahuelEspinosa6 <nahuelespinosa6@gmail.com>
2023-02-19 17:36:23 -0300

    ejercicios hechos

--- 6-InstruccionFor/jsIteracionesFor-01.js
+++ 6-InstruccionFor/jsIteracionesFor-01.js
@@ -1,4 +1,10 @@
 function mostrar()
 {
-	alert("ok");
+	/* al presionar el botón mostrar 10 repeticiones con números ASCENDENTE, desde el 1 al 10. */
+
+	let i;
+
+	for(i = 1; i < 11; i++){
+		console.log(i);
+	}
 }
